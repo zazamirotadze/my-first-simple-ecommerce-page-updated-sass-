75aa433ab572e108d3de99fe0caeb80e86faf64b
zaza mirotadze <mirotadzezaza1@gmail.com>
2022-11-23 08:56:04 +0400

    solved images problem

--- src/Data.js
+++ src/Data.js
@@ -6,19 +6,19 @@ const Data =
         {
             id:1,
             name: "booo x",
-            img: "/static/media/blank-notepad-4-1244263.d21c9a5eac72816cdcca.jpg",
+            img: "blank-notepad-4-1244263.jpg",
             price: 20
         },
         {
             id:2,
             name: "Booook y",
-            img: "static/media/my-address-book-icon-1142969.c4fabc608ebf06c58207.jpg",
+            img: "my-address-book-icon-1142969.jpg",
             price: 120
         },
         {
             id:3,
             name: "Boooookky Z",
-            img: "/static/media/paper-pen-color-1634002.72a960f92637b6b94e36.jpg", 
+            img: "paper-pen-color-1634002.jpg", 
             price: 90
         }
     ]
