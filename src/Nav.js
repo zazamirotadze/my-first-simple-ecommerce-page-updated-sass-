6aa938c8cdb498dc73d3753415ee565068ddcd2b
zaza mirotadze <mirotadzezaza1@gmail.com>
2022-11-25 12:59:45 +0400

    add media queries

--- src/Nav.js
+++ src/Nav.js
@@ -11,16 +11,22 @@ const Nav = ({numberOfItems, fullSum, setNumberOfItems, setCartItems, setFullSum
     <div className='nav'>
         <h1>შეიძინე წიგნები ჩვენთან</h1>
         <div className='nav__menu-div'>
-           <div className='nav__books-btn-div'>
-                <Link to="/" style={{width: "3em"}} ><button  className="nav__books-btn">წიგნები</button></Link>
-            </div> 
-        
-            <div className='nav__cart-icon-div'>
-                <Link to="/cart"  style={{width: "3.5em"}  }> 
-                        <i className='fa fa-shopping-cart nav__cart-icon' />
-                </Link>
-                <div className='nav__numberOfItems'><p>{numberOfItems}</p></div>
+            <div className='nav__first-part'>
+                <div className='nav__books-btn-div'>
+                    <Link to="/" style={{width: "3em"}} ><button  className="nav__books-btn">წიგნები</button></Link>
+                </div> 
+                
+            
+                <div className='nav__cart-icon-div'>
+                    <div className='nav__cart-icon'>
+                        <Link to="/cart"  style={{width: "3.5em"}  }> 
+                                <i className='fa fa-shopping-cart ' />
+                        </Link>
+                    </div>
+                    <div className='nav__numberOfItems'><p>{numberOfItems}</p></div>
+                </div>
             </div>
+
             <div className='nav__payment-div'>
                 <p> გადასახდელი ჯამური თანხა: {fullSum} ₾</p>
                 <button onClick={sendPayment} className="nav__payment-btn">გადახდა</button>
